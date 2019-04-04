e9e5c635da69c65189ffe6ed5e67fd2c0731269f
Sean Budd <seanb@pageuppeople.com>
2019-04-04 11:03:10 +1100

    repackage alembic

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(name='dpo',
       install_requires=[
           'psycopg2-binary==2.7.7',
           'SQLAlchemy==1.2.17',
-          'alembic==1.0.8'
-      ]
+          'alembic==1.0.8',
+      ],
+      package_data={
+          '': ['alembic.ini', 'alembic/*.py', 'alembic/**/*.py'],
+      },
       )
