c507ecd0ae9e8d69547ebcacd686625a26c54f14
Sean Budd <seanbudd123@gmail.com>
2019-04-08 12:10:31 +1000

    bump version number up for next release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dpo',
-      version='0.1.0',
+      version='0.1.1',
       packages=find_packages(),
       install_requires=[
           'psycopg2-binary==2.7.7',
