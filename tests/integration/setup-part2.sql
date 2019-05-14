af6cf479f77f07e620dd210d2973aa6f0907f076
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-05-14 13:50:47 +1000

    add schema usage permission to test user

--- tests/integration/setup-part2.sql
+++ tests/integration/setup-part2.sql
@@ -1,3 +1,4 @@
 -- grant all access to test user within app schema
+GRANT USAGE ON SCHEMA dpo TO integration_test_user;
 GRANT ALL PRIVILEGES ON ALL tables IN SCHEMA dpo TO integration_test_user;
 GRANT ALL PRIVILEGES ON ALL sequences IN SCHEMA dpo TO integration_test_user;
